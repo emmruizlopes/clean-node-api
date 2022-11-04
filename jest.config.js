38666d0ec5ec0a0bee13a0a020d98473784ab17e
Emmanuel Ruiz Lopes <emmruizlopes@gmail.com>
2022-11-04 09:09:46 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
   }
 }
--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -20,9 +20,12 @@ describe('Bcrypt Adapter', () => {
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('Should return a hash on success', async () => {
+  test('Should throw if bcrypt throws', async () => {
     const sut = makeSut()
-    const hash = await sut.encrypt('any_value')
-    expect(hash).toBe('hash')
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 })
